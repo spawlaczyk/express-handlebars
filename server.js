577b41a8c6375e6f4d312dbf803deaf57889ccb1
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-05-31 18:27:11 +0200

    Finish

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname.replace(/\.[^/\\.]+$/, "") + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 
@@ -8,6 +20,7 @@ app.engine('.hbs', hbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +44,18 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const image = req.file;
+
+  if(author && sender && title && message && image) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
